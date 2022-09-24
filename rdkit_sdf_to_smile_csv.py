846be1a654ec772727686f4ff76aebd312e47146
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-09-24 13:50:40 +0300

    Add files via upload

---
+++ rdkit_sdf_to_smile_csv.py
@@ -0,0 +1,21 @@
+#conda env list
+#conda activate my-rdkit-env 
+from rdkit import Chem
+from rdkit.Chem.ChemUtils import SDFToCSV
+import os
+import pandas as pd 
+names=[]
+smile_format=[]
+file_names=os.listdir("sdf_dir")
+for file_name in file_names:
+    sdf = Chem.SDMolSupplier( 'sdf_dir/%s'%file_name )
+    for mol in sdf:
+        smi = Chem.MolToSmiles(mol)
+        print(file_name, smi)
+        names.append(file_name)
+        smile_format.append(smi)
+df_results=pd.DataFrame()
+df_results["molecules_names"]=names
+df_results["smiles_formats"]=smile_format
+df_results.to_csv("all_csmiles_format.csv")
+
